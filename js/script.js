70c1f9e980dce05685a5390fb4c20f6e176439c0
PedroPenha <pedro.eng98@gmail.com>
2023-03-01 10:07:51 -0300

    js_error_nnavItem

---
+++ js/script.js
@@ -0,0 +1,52 @@
+class MobileNavBar{
+    constructor(mobileMenu, navList, navItem){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navItem = document.querySelectorAll(navItem);
+    
+        this.activeClass = "active";
+        this.animateLinks();
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ?(link.style.animation = "")
+            :(link.style.animation= `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+   }
+
+   handleClick(){
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+   }
+   
+   
+   addClickEvent(){
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    Infinity(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+
+
+
+
+
+
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".nav-menu",
+    ".nav flex-column",
+    ".nav flex-column li",
+);
+
+
+mobileNavBar.Infinity();
+
+
