ddb5bf3c477c9788e1fb875737038034fd17b1a2
PedroPenha <pedro.eng98@gmail.com>
2023-03-03 15:24:45 -0300

    navBar_responsive_Concluded

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
     ?menuMobile.classList.replace("bi-list", "bi-x")
     :menuMobile.classList.replace("bi-x", "bi-list")
+    body.classList.toggle("menu-nav-active")
 })
 
 
